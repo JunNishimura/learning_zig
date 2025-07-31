71c8e101f7279f16c32b24585cc6469a45e04ae9
Jun Nishimura <n.junjun0303@gmail.com>
2025-07-31 23:35:15 +0900

    chore: recursive pointer

--- pointers/main.zig
+++ pointers/main.zig
@@ -1,35 +1,26 @@
 const std = @import("std");
 
 pub fn main() void {
-    // var user = User{
-    //     .id = 1,
-    //     .power = 100,
-    // };
-
-    // user.levelUp();
-    // std.debug.print("User {d} has power {d}\n", .{ user.id, user.power });
-
-    var name = [4]u8{ 'G', 'o', 'k', 'u' };
-    const user = User{
+    const leto = User{
         .id = 1,
         .power = 100,
-        .name = name[0..],
+        .manager = null,
     };
-    levelUp(user);
-    std.debug.print("{s}\n", .{user.name});
+
+    const duncan = User{
+        .id = 2,
+        .power = 50,
+        .manager = &leto,
+    };
+
+    std.debug.print("{any}\n{any}", .{
+        leto,
+        duncan,
+    });
 }
 
-fn levelUp(user: User) void {
-    user.name[2] = '!';
-}
-
-pub const User = struct {
+const User = struct {
     id: u64,
     power: i32,
-    name: []u8,
-
-    fn levelUp(user: *User) void {
-        std.debug.print("{*}\n{*}\n", .{ &user, user });
-        user.power += 1;
-    }
+    manager: ?*const User,
 };
