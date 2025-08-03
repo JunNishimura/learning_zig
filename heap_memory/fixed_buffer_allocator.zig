1396f7eba247bc601b178fbb2ef8595b92add6eb
Jun Nishimura <n.junjun0303@gmail.com>
2025-08-03 20:11:59 +0900

    feat: fixed buffer allocator

---
+++ heap_memory/fixed_buffer_allocator.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var buf: [150]u8 = undefined;
+    var fa = std.heap.FixedBufferAllocator.init(&buf);
+
+    defer fa.reset();
+
+    const allocator = fa.allocator();
+
+    const json = try std.json.stringifyAlloc(allocator, .{
+        .this_is = "an anonymous struct",
+        .above = true,
+        .last_param = "are options",
+    }, .{ .whitespace = .indent_2 });
+
+    defer allocator.free(json);
+
+    std.debug.print("{s}\n", .{json});
+}
