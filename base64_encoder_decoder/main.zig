c813d0eb6befb04bb6607bb84599c989c11955bc
Jun Nishimura <n.junjun0303@gmail.com>
2025-11-07 00:06:21 +0900

    feat: implement Base64 decoding functionality and update main function

--- base64_encoder_decoder/main.zig
+++ base64_encoder_decoder/main.zig
@@ -5,8 +5,18 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-    const bits = 0b10010111;
-    try stdout.print("{d}\n", .{bits & 0b00110000});
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+
+    const text = "Testing some more stuff";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
+
+    const base64 = Base64.init();
+    const encoded_text = try base64.encode(allocator, text);
+    const decoded_text = try base64.decode(allocator, etext);
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
+    try stdout.print("Decoded text: {s}\n", .{decoded_text});
     try stdout.flush();
 }
 
@@ -55,6 +65,19 @@ const Base64 = struct {
         return self._table[index];
     }
 
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=')
+            return 64;
+        var index: u8 = 0;
+        for (0..63) |i| {
+            if (self._char_at(i) == char)
+                break;
+            index += 1;
+        }
+
+        return index;
+    }
+
     pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
         if (input.len == 0) {
             return "";
@@ -96,5 +119,36 @@ const Base64 = struct {
         }
 
         return out;
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_output = try _calc_decode_length(input);
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buf = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buf[count] = self._char_index(input[i]);
+            count += 1;
+
+            if (count == 4) {
+                output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+                if (buf[2] != 64) {
+                    output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+                }
+                if (buf[3] != 64) {
+                    output[iout + 2] = (buf[2] << 6) + buf[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+
+        return output;
     }
 };
